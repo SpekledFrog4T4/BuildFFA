eb6b6998c21fc4824583b75c5a269d955452eabb
SpekledFrog4T4 <96526043+SpekledFrog4T4@users.noreply.github.com>
2021-12-22 04:31:45 -0800

    Add files via upload

---
+++ buildffa/src/SpekledFrog/buildffa/BlockTask.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SpekledFrog\buildffa;
+
+
+use SpekledFrog\buildffa\Main;
+use pocketmine\scheduler\Task;
+use pocketmine\block\Block;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\block\BlockFactory;
+
+class BlockTask extends Task {
+
+
+   public $main;
+   public $block;
+
+   public function __construct(Main $main, Block $block){
+
+      $this->main = $main;
+      $this->block = $block;
+   }
+  
+
+    public function onRun(int $CurrentTick){
+       $this->block->getLevel()->setBlock($this->block, Block::get(0));
+
+
+    }
+
+
+}
---
+++ buildffa/src/SpekledFrog/buildffa/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SpekledFrog\buildffa;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\block\BlockFactory;
+
+class Main extends PluginBase implements Listener {
+  
+   public function onEnable(){
+   	  $this->getlogger()->info("plugin Enabled");
+   	  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+   }
+
+   public function onDisable(){
+   	  $this->getlogger()->info("plugin Disabled");
+   }
+
+   public function onPlace(BlockPlaceEvent $event){
+   	  $block = $event->getBlock();
+   	if($block->getId() == 24){
+   	   $event->setCancelled();
+
+       $world = $block->getLevel();
+       $world->setBlock($block, BlockFactory::get(id: 24, meta: 0, pos: null));
+       $this->getScheduler()->scheduleDelayedTask(new BlockTask($this, $block), delay: 20 * 5);
+
+   	}
+
+   }
+ }
+
